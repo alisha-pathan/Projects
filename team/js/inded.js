37913ad864bde429b8a103d03c48dafa61f60353
alishapatha <alishapathan401@gmail.com>
2025-02-20 22:34:18 +0530

    Add files via upload

---
+++ team/js/inded.js
@@ -0,0 +1,57 @@
+// let Team = {
+//     DC: {
+//         player: [ 
+//             {
+//                 name: "Virat Kohli",
+//                 age: 36,
+//                 img: "virat.png"
+//             },
+//             {
+//                 name: "Jusprit Bumrah",
+//                 age: 31,
+//                 img: "bumrah.png"
+//             },
+//             {
+//                 name: "Ravindra Jadeja",
+//                 age: 36,
+//                 img: "jadeja.png"
+//             },
+//             {
+//                 name: "Rohit Sharma",
+//                 age: 36,
+//                 img: "rohit.png"
+//             }
+//         ]
+//     },
+//     Csk: {
+
+//     },
+//     hydrabadHeros: {
+
+//     },
+//     KKR: {
+
+//     },
+// }
+
+let TeamData = [
+    {
+        Tname: "Hyderabad Heroes",
+        Tlog: "https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/cc16a2137130043.620569b593ddd.png",
+        contact: "Local Champions"
+    },
+    {
+        Tname: "Chennai Kings",
+        Tlog: "https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/cc16a2137130043.620569b593ddd.png",
+        contact: "Southern Warriors"
+    },
+    {
+        Tname: "Mumbai Titans",
+        Tlog: "https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/cc16a2137130043.620569b593ddd.png",
+        contact: "Coastal Legends"
+    }
+];
+
+
+
+
